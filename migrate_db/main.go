58a0ad0f7c625bb75b521f4148d17cbd68b96a9b
Alex Nichol <unixpickle@gmail.com>
2015-08-29 20:27:49 -0400

    implemented migration tool

--- migrate_db/main.go
+++ migrate_db/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"labix.org/v2/mgo"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/unixpickle/1mage.us/thumbnail"
+	"labix.org/v2/mgo"
 )
 
 type Image struct {
-	MIME string `json:"mimeType" bson:"mime"`
-	Seq  int    `json:"id" bson:"sequence"`
+	MIME      string `json:"mimeType" bson:"mime"`
+	Seq       int    `json:"id" bson:"sequence"`
+	Timestamp int64  `json:"timestamp"`
+
+	HasSize bool `json:"has_size"`
+	Width   int  `json:"width"`
+	Height  int  `json:"height"`
+
+	HasThumbnail    bool `json:"has_thumbnail"`
+	ThumbnailWidth  int  `json:"thumb_width"`
+	ThumbnailHeight int  `json:"thumb_height"`
+}
+
+type ImageDb struct {
+	Images    []Image `json:"images"`
+	CurrentId int     `json:"current_id"`
 }
 
 func die(args ...interface{}) {
@@ -17,8 +41,17 @@ func die(args ...interface{}) {
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		die("Usage: migrate_db <images path>")
+	if len(os.Args) != 3 {
+		die("Usage: migrate_db <images path> <output path>")
+	}
+
+	imagesPath := os.Args[1]
+	outputPath := os.Args[2]
+
+	if info, err := os.Stat(outputPath); err != nil {
+		die(err)
+	} else if !info.IsDir() {
+		die("Given output path is not a directory.")
 	}
 
 	session, err := mgo.Dial("mongodb://127.0.0.1:27017/1mage")
@@ -31,6 +64,70 @@ func main() {
 		die(err)
 	}
 
-	// TODO: parse the dates from the path argument.
-	fmt.Println(result)
+	var maxId int
+	for i := range result {
+		imageInfo := &result[i]
+
+		fmt.Println("Processing", imageInfo.Seq)
+		if imageInfo.Seq > maxId {
+			maxId = imageInfo.Seq
+		}
+
+		imagePath := filepath.Join(imagesPath, strconv.Itoa(imageInfo.Seq))
+
+		f, err := os.Open(imagePath)
+		if err != nil {
+			die(err)
+		}
+		defer f.Close()
+
+		if stats, err := f.Stat(); err != nil {
+			die(err)
+		} else {
+			imageInfo.Timestamp = stats.ModTime().Unix()
+		}
+
+		newImagePath := filepath.Join(outputPath, strconv.Itoa(imageInfo.Seq))
+		thumbnailPath := filepath.Join(outputPath, strconv.Itoa(imageInfo.Seq)+
+			"_thumb")
+
+		if err := copyFile(imagePath, newImagePath); err != nil {
+			die(err)
+		}
+
+		img, _, err := image.Decode(f)
+		if err != nil {
+			fmt.Println("Notice: could not decode image: " + imagePath)
+			continue
+		}
+		imageInfo.HasSize = true
+		imageInfo.Width = img.Bounds().Dx()
+		imageInfo.Height = img.Bounds().Dy()
+
+		thumb, width, height := thumbnail.MakeThumbnail(img)
+		imageInfo.HasThumbnail = true
+		imageInfo.Width = width
+		imageInfo.Height = height
+		if err := ioutil.WriteFile(thumbnailPath, thumb, 0700); err != nil {
+			die(err)
+		}
+	}
+
+	var db ImageDb
+	db.Images = result
+	db.CurrentId = maxId + 1
+	dbPath := filepath.Join(outputPath, "db.json")
+	if dbData, err := json.Marshal(db); err != nil {
+		die(err)
+	} else if err = ioutil.WriteFile(dbPath, dbData, 0700); err != nil {
+		die(err)
+	}
+}
+
+func copyFile(source, dest string) error {
+	contents, err := ioutil.ReadFile(source)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(dest, contents, 0700)
 }
