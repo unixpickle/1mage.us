2ddb1e7459bf167d3fa883237c6eec90b7608e45
Alex Nichol <unixpickle@gmail.com>
2015-09-09 15:45:47 -0400

    drafted rate limits

---
+++ rate_limits.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+var rateLimitHostTable map[string]int = map[string]int{}
+var rateLimitLastTime time.Time = time.Now()
+
+// RateLimitRequest should be called once per uploaded image.
+// This will return true if the requester has reached its rate limit.
+func RateLimitRequest(r *http.Request) bool {
+	// TODO: in the future, there will be some sort of cookie to bypass rate limiting.
+
+	now := time.Now()
+	if rateLimitLastTime.Add(time.Hour).Before(now) || rateLimitLastTime.Hour() != now.Hour() {
+		rateLimitHostTable = map[string]int{}
+	}
+
+	host := r.RemoteAddr
+	if forwardHeader := r.Header.Get("X-Forwarded-For"); forwardHeader != "" {
+		forwardHosts := strings.Split(forwardHeader, ", ")
+		host = forwardHosts[0]
+	}
+
+	if count, ok := rateLimitHostTable[host]; !ok {
+		rateLimitHostTable[host] = 1
+		return 1 <= rateLimitMaxCount()
+	} else {
+		rateLimitHostTable[host] = count+1
+		return count < rateLimitMaxCount()
+	}
+}
+
+func rateLimitMaxCount() int {
+	GlobalDatabase.RLock()
+	defer GlobalDatabase.RUnlock()
+	return GlobalDatabase.Config.MaxCountPerHour
+}
